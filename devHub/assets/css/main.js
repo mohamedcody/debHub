af2c48993e974b32e5646d9243141b1217b76636
mohamedcody <mohamedcody18@gmail.com>
2026-09-01 18:23:54 +0300

    updat

--- devHub/assets/css/main.js
+++ devHub/assets/css/main.js
@@ -1,3 +1,4 @@
+// the button named tools how to hte show the style in the click the show 
 const toolsBtn = document.getElementById('tools-btn');
 const toolsMenu = document.getElementById('sub-menu');
 
